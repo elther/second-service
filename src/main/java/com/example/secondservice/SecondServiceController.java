acaa69d5d5d897d15180f6ac00a97624ce6f0f2d
elther <elther@naver.com>
2022-04-09 23:34:21 +0900

    refactor : - add server port for logging (check api)

--- src/main/java/com/example/secondservice/SecondServiceController.java
+++ src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,15 +1,24 @@
 package com.example.secondservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/second-service")
 @Slf4j
 public class SecondServiceController {
+    Environment env;
+
+    public SecondServiceController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("welcome")
     public String welcome(){
         return "Welcome to the second service";
@@ -22,7 +31,9 @@ public class SecondServiceController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there. this is a message from second service";
+    public String check(HttpServletRequest request){
+        log.info("server port={}", request.getServerPort());
+        return String.format("Hi, there. this is a message from second service %s"
+                , env.getProperty("local.server.port"));
     }
 }
